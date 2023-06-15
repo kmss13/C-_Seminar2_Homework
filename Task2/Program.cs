e10f9db36519e975f2f57906cd0faa9b200ea716
Viking <kmss13@mail.ru>
2023-06-15 22:30:25 +0400

    add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int ReadNumber(string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+bool ValidateThree (int number)
+{
+    if (number >=100)
+    {
+        return true;
+    }
+    Console.WriteLine($"В числе {number} меньше трех цифр");
+    return false;
+}
+
+int num = ReadNumber("Введите число ");
+
+if(ValidateThree(num))
+{
+    while (num > 999) 
+    {
+        num = num/ 10;
+    }
+    int thirdNum = num % 10;
+    
+Console.WriteLine($"Третья цифра числа  равна {thirdNum}");
+
+}
+
+
+
+
+
+
